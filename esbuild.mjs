6ee421d42ce5025526932fe4a2f09c04ae939492
Felipe Figueroa <amenadiel@gmail.com>
2021-09-28 00:29:14 -0300

    cjs build

--- esbuild.mjs
+++ esbuild.mjs
@@ -4,39 +4,53 @@ import esbuild from 'esbuild'
 import fs from 'fs/promises'
 import NodeModulesPolyfills from '@esbuild-plugins/node-modules-polyfill'
 import GlobalsPolyfills from '@esbuild-plugins/node-globals-polyfill'
+//import { externalGlobalPlugin } from "esbuild-plugin-external-global"
+
 console.log({GlobalsPolyfills,NodeModulesPolyfills})
 const mode = process.env.NODE_ENV || 'production'
 /**
- * @type {import('esbuild').BuildOptions}
+ * @returns {import('esbuild').BuildOptions}
  */
-let buildOptions={
+const buildOptions=({outdir,format,outExtension})=>{
+    return {
     bundle: true,
- platform:'browser',
+    platform:'browser',
     write: true,
     metafile: true,
     entryPoints: [ 'src/index.ts'],
-    outExtension:{'.js':'.mjs'},
-    outdir:'dist',
-     format:'esm',
-     treeShaking: true,
-     minifySyntax: true,
-     minifyIdentifiers: true,
+    outExtension,
+    outdir,
+    format,
+    treeShaking: true,
+    minifySyntax: true,
+    minifyIdentifiers: true,
     sourcemap: mode!=="production",
-    minify:false &&mode==="production",
+    minify:mode==="production",
     target:'es2020',
     //plugins: [
          //NodeModulesPolyfills.default(),
         //GlobalsPolyfills.default({
-            //process: true,
+            //stream: true,
         //}),
+        //externalGlobalPlugin( { 'Buffer': 'fetch' })   
     //],
 
 }
+}
 esbuild
-.build(buildOptions)
+.build(buildOptions({format:'esm',outdir:'dist',outExtension:{'.js':'.mjs'}}))
 .then(result=>{
     console.log({...buildOptions,mode,resultkeys:Object.keys(result)})
     return fs.writeFile('meta.json',  JSON.stringify(result.metafile))
+})
+.then(()=>{
+    return esbuild
+    .build(buildOptions({format:'cjs',outdir:'dist/cjs'}))
+
+})
+.then(result=>{
+    console.log({...buildOptions,mode,resultkeys:Object.keys(result)})
+    return fs.writeFile('meta.cjs.json',  JSON.stringify(result.metafile))
 })
 .catch((err) => {
     console.error(err)
