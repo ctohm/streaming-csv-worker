5470c2696a9af243a0405b8f25cd1e50ce174c09
Felipe Figueroa <amenadiel@gmail.com>
2021-12-22 06:20:12 -0300

    preparing for pages

--- docs/files/[path].ts
+++ docs/files/[path].ts
@@ -9,9 +9,17 @@ export async function onRequestOptions({ env, request }) {
         },
     });
 }
+function getHugeCSV(req: Request): Request {
+
+    let newReq = new Request("https://www.ishares.com/us/products/239756/ishares-growth-allocation-etf/1467271812596.ajax?fileType=csv&fileName=AOR_holdings&dataType=fund", req);
+    newReq.headers.set('cache-control', 'no-cache');
+    return newReq;
+
+}
+
 export async function onRequestGet({ env, request }) {
 
-    let response = await env.ASSETS.fetch(request)
+    let response = await fetch(getHugeCSV(request))
     response = new Response(response.body, response)
     response.headers.set("Cache-Control", "no-cache, no-store")
     response.headers.set("Access-Control-Allow-Origin", "*")
