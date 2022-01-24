ae75dd647162ffb979299958810bbcd9d753a2cf
Felipe Figueroa <amenadiel@gmail.com>
2022-01-23 22:36:37 -0300

    moves (removes) everything on src

--- src/lib/index.ts
+++
@@ -1,5 +0,0 @@
-export { StreamingCSVParser } from './StreamingCSVParser'
-import { FetchPapaStreamer, TransformStreamer, default as Papa } from './TransformStreamer'
-
-export { FetchPapaStreamer, TransformStreamer }
-export default Papa
